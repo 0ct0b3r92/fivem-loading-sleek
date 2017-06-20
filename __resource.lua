4af86f7509510d4590a123c63c94937525badf6a
NicolasStr_ <nicolas.streng@gmail.com>
2017-06-20 18:44:41 +0200

    baguette

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+--
+-- Author: @NicolasStr_
+-- If you have any issue, please consider opening a issue on Github
+--
+files {
+    'files/index.html',
+    'files/lib/script.js',
+    'files/lib/style.css',
+}
+
+loadscreen 'files/index.html'
